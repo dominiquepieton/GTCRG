6c0f95dcb7fd3119444aa05f0626ac29039f8f11
Dominique PIETON <dpieton@talaron.com>
2024-01-04 15:12:48 +0100

    T0002,T0005,T0007,T0008 for User

---
+++ Maquette/index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport"    content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+	
+	<title>GTCRG</title>
+
+	<link rel="shortcut icon" href="assets/images/gt_favicon.png">
+	
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="Assets/css/font-awesome.min.css">
+
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="Assets/css/bootstrap-theme.css" media="screen" >
+	<link rel="stylesheet" href="Assets/css/main.css">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body class="home">
+    <!----------------- NAVBAR Fixed -------------------------------->
+    <div class="navbar navbar-inverse navbar-fixed-top headroom" >
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+				<a class="navbar-brand" href="index.php">GTCRG</a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">About</a></li>
+                    <li><a href="#faq">F.A.Q</a></li>
+					<li><a href="">Contact</a></li>
+					<li><a class="btn" href="login.php">LOGIN / ENRG.</a></li>
+				</ul>
+			</div><!--/.nav-collapse -->
+		</div>
+	</div>
+    <!-- Header -->
+	<header id="head">
+		<div class="container">
+			<div class="row">
+				<h1 class="lead"><b>Console, Jeux, Achat prévisionnelle...</b></h1>
+				<p class="tagline"><b>GTCRG : gestion de Collection Retro-Gaming</b></p>
+				<p><a class="btn btn-action btn-lg" role="button" href="#about">En savoir plus</a></p>
+			</div>
+		</div>
+	</header>
+    <!-- Intro -->
+	<div class="container text-center" id="about">
+		<br> <br>
+		<h2 class="thin">L'application est conçu pour vous simpifier votre temps de gestion.</h2>
+		<p class="text-muted">
+			Exporter votre collection en fichier EXCEL ou PDF en un click :<br> 
+			Enfin une applicaton simple et efficace.
+		</p>
+	</div>
+    <!-- Highlights - jumbotron -->
+	<div class="jumbotron top-space">
+		<div class="container">
+			<h3 class="text-center thin">Les Fonctionnalités : </h3>
+			<div class="row">
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-cogs fa-5"></i>Enregistrer vos consoles et jeux</h4></div>
+					<div class="h-body text-center">
+						<p>Sauvegarder vos jeux et vos consoles, visualisez la liste de vos jeux par console, éditeur, constructeur...</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-flash fa-5"></i>Exporter vos données</h4></div>
+					<div class="h-body text-center">
+						<p>Choissisez le type de fichier Excel, Pdf en un click tous vos jeux enregistrés sont exportés dans le fichier de votre choix.</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-heart fa-5"></i>Créer une liste de vos futurs Achats</h4></div>
+					<div class="h-body text-center">
+						<p>Vous avez un budget pour vos futurs achats de jeux vidéos, grace à la fonctionnalité futurs achats vous aurez la possibilité
+                            d'ajouter des jeux dans le paniers et d'aller les acheter directement sur le site du revendeur cela vous évite la douloureuse recherche sur plusieurs
+                            revendeur et de comparer le prix. Vous aurez le prix moyen par console de chaque sites. 
+                        </p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 highlight">
+					<div class="h-caption"><h4><i class="fa fa-smile-o fa-5"></i>Gérer le prêt de vos jeux</h4></div>
+					<div class="h-body text-center">
+						<p>Vous avez prêté des jeux à des amis(es) et vous ne savez plus à qui ou quelle jeu. La fonctionnalité prêt de jeux est la pour vous aider.</p>
+					</div>
+				</div>
+			</div> <!-- /row  -->
+		
+		</div>
+	</div>
+    <!-- container -->
+	<div class="container">
+
+<h2 class="text-center top-space">Questions Fréquentes</h2>
+<br>
+
+<div class="row" id="faq">
+    <div class="col-sm-6">
+        <h3>L'application est-elle simple à utilisé ?</h3>
+        <p>Oui, elle a été conçue pour que cela soit facile et que tous le monde puissent l'utiliser.</p>
+    </div>
+    <div class="col-sm-6">
+        <h3>Puis-je facilement fermer mon compte ou en créer un?</h3>
+        <p> Oui, vous pouvez fermer votre compte sur simple demande à l'adresse contact@GTCRG.com. 
+            Pour la création d'un compte rien de plus simple, vous remplissez le formulaire et vous pouvez l'utiliser aussitôt en vous connectant.
+        </p>
+    </div>
+</div> <!-- /row -->
+
+<div class="row">
+    <div class="col-sm-6">
+        <h3>Peut-on gérer son profil depuis l'application?</h3>
+        <p>
+            Oui, vous avez une page profil et vous pourrez gérer les fichiers exportés, votre profil.... 
+        </p>
+    </div>
+    <div class="col-sm-6">
+        <h3>L'application est-elle payante?</h3>
+        <p>Non, l'application n'est pas payante car nous sommes à la version beta.</p>
+    </div>
+</div> <!-- /row -->
+
+<div class="jumbotron top-space">
+    <h4>Dicta, nostrum nemo soluta sapiente sit dolor quae voluptas quidem doloribus recusandae facere magni ullam suscipit sunt atque rerum eaque iusto facilis esse nam veniam incidunt officia perspiciatis at voluptatibus. Libero, aliquid illum possimus numquam fuga.</h4>
+     <p class="text-right"><a class="btn btn-primary btn-large">Learn more »</a></p>
+  </div>
+
+</div>
+
+<footer id="footer" class="top-space">
+
+		<div class="footer1">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Contact</h3>
+						<div class="widget-body">
+							<p>+000 000 000 000<br>
+								<a href="mailto:#">GTCRG@blibli.com</a><br>
+								<br>
+								Montpellier, FRANCE
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-3 widget">
+						<h3 class="widget-title">Follow me</h3>
+						<div class="widget-body">
+							<p class="follow-me-icons">
+								<a href=""><i class="fa fa-twitter fa-2"></i></a>
+								<a href=""><i class="fa fa-dribbble fa-2"></i></a>
+								<a href=""><i class="fa fa-github fa-2"></i></a>
+								<a href=""><i class="fa fa-facebook fa-2"></i></a>
+							</p>	
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<h3 class="widget-title">Text widget</h3>
+						<div class="widget-body">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
+							<p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+					
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="simplenav">
+								<a href="#">Home</a> | 
+								<a href="#about">About</a> |
+								<b><a href="login.php">Sign up</a></b>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 widget">
+						<div class="widget-body">
+							<p class="text-right">
+								Copyright &copy; 2024, GTCRG.</a> 
+							</p>
+						</div>
+					</div>
+
+				</div> <!-- /row of widgets -->
+			</div>
+		</div>
+
+	</footer>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/headroom.min.js"></script>
+	<script src="assets/js/jQuery.headroom.min.js"></script>
+	<script src="assets/js/template.js"></script>
+</body>
+</html>
---
+++ Maquette/login.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author"      content="Sergey Pozhilov (GetTemplate.com)">
+        
+        <title>GTCRG</title>
+
+        <link rel="shortcut icon" href="assets/images/gt_favicon.png">
+        
+        <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+        <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="Assets/css/font-awesome.min.css">
+
+        <!-- Custom styles for our template -->
+        <link rel="stylesheet" href="Assets/css/bootstrap-theme.css" media="screen" >
+        <link rel="stylesheet" href="Assets/css/main.css">
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="assets/js/html5shiv.js"></script>
+        <script src="assets/js/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top headroom" >
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- Button for smallest screens -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+                    <a class="navbar-brand" href="index.php">GTCRG</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav pull-right">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#about">F.A.Q</a></li>
+                        <li><a href="">Contact</a></li>
+                        <li><a class="btn" href="login.php">LOGIN / ENRG.</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+
+        <header id="head" class="secondary"></header>
+
+	<!-- container -->
+	<div class="container">
+
+		<ol class="breadcrumb mt-5">
+			<li><a href="index.php">Retour</a></li>
+			
+		</ol>
+
+		<div class="row">
+			
+			<!-- Article main content -->
+			<article class="col-xs-12 maincontent">
+				<!--<header class="page-header">
+					<h1 class="page-title">Connexion</h1>
+				</header>-->
+				
+				<div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+					<div class="panel panel-default">
+						<div class="panel-body">
+							<h3 class="thin text-center">Connectez-vous à votre compte</h3>
+							<p class="text-center text-muted">Vous n'avez pas de compte cliquez sur le lien : <a href="register.php">Register</a> .</p>
+							<hr>
+							
+							<form>
+								<div class="top-margin">
+									<label>Username/Email <span class="text-danger">*</span></label>
+									<input type="text" class="form-control" name="login">
+								</div>
+								<div class="top-margin">
+									<label>Password <span class="text-danger">*</span></label>
+									<input type="password" class="form-control" name="password">
+								</div>
+								<hr>
+								<div class="row">
+									<div class="col-lg-4">
+										<!--<b><a href="">Forgot password?</a></b>-->
+									</div>
+									<div class="col-lg-8 text-start">
+										<button class="btn btn-action" type="submit">Sign in</button>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+
+				</div>
+				
+			</article>
+			<!-- /Article -->
+
+		</div>
+	</div>
+
+        <footer id="footer" class="top-space">
+            <div class="footer1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-3 widget">
+                            <h3 class="widget-title">Contact</h3>
+                            <div class="widget-body">
+                                <p>+000 000 000 000<br>
+                                    <a href="mailto:#">GTCRG@blibli.com</a><br>
+                                    <br>
+                                    Montpellier, FRANCE
+                                </p>	
+                            </div>
+                        </div>
+                        <div class="col-md-3 widget">
+                            <h3 class="widget-title">Follow me</h3>
+                            <div class="widget-body">
+                                <p class="follow-me-icons">
+                                    <a href=""><i class="fa fa-twitter fa-2"></i></a>
+                                    <a href=""><i class="fa fa-dribbble fa-2"></i></a>
+                                    <a href=""><i class="fa fa-github fa-2"></i></a>
+                                    <a href=""><i class="fa fa-facebook fa-2"></i></a>
+                                </p>	
+                            </div>
+                        </div>
+                        <div class="col-md-6 widget">
+                            <h3 class="widget-title">Text widget</h3>
+                            <div class="widget-body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, dolores, quibusdam architecto voluptatem amet fugiat nesciunt placeat provident cumque accusamus itaque voluptate modi quidem dolore optio velit hic iusto vero praesentium repellat commodi ad id expedita cupiditate repellendus possimus unde?</p>
+                                <p>Eius consequatur nihil quibusdam! Laborum, rerum, quis, inventore ipsa autem repellat provident assumenda labore soluta minima alias temporibus facere distinctio quas adipisci nam sunt explicabo officia tenetur at ea quos doloribus dolorum voluptate reprehenderit architecto sint libero illo et hic.</p>
+                            </div>
+                        </div>
+                    </div> <!-- /row of widgets -->
+                </div>
+            </div>
+
+            <div class="footer2">
+                <div class="container">
+                    <div class="row"> 
+                        <div class="col-md-6 widget">
+                            <div class="widget-body">
+                                <p class="simplenav">
+                                    <a href="#">Home</a> | 
+                                    <a href="#about">About</a> |
+                                    <b><a href="signup">Sign up</a></b>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="col-md-6 widget">
+                            <div class="widget-body">
+                                <p class="text-right">
+                                    Copyright &copy; 2024, GTCRG.</a> 
+                                </p>
+                            </div>
+                        </div>
+                    </div> <!-- /row of widgets -->
+                </div>
+            </div>
+        </footer>
+
+<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="assets/js/headroom.min.js"></script>
+<script src="assets/js/jQuery.headroom.min.js"></script>
+<script src="assets/js/template.js"></script>
+    </body>
+</html>
